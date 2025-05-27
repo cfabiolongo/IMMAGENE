19eec0737ceabafbcc81ee29b0f041de43393c44
fabio <fabiuslongo@gmail.com>
2025-05-27 16:13:52 +0200

    +mas

---
+++ front_end_mas.py
@@ -0,0 +1,83 @@
+import time
+from actions import *
+
+# ---------------------------------------------------------------------
+# PHIDIAS rules variable declaration
+# ---------------------------------------------------------------------
+
+def_vars("X", "Y", "D", "H", "Z", "L", "M", "A", "D", "W")
+
+# ---------------------------------------------------------------------
+# Agents section
+# ---------------------------------------------------------------------
+
+def create_custom_agent(class_name):
+    def main(self):
+        # Custom intention
+        +TASK(X)[{'from': A}] >> [show_line("\nReceived belief TASK(",X,") from ", A), +TRIPLE(X,X,X), +TASK(X)[{'to': 'main'}]]
+
+    return type(class_name, (Agent,), {"main": main})
+
+
+# custom agent rocco
+globals()["rocco"] = create_custom_agent("rocco")
+instance = globals()["rocco"]()
+
+
+# ---------------------------------------------------------------------
+# Agent 'main'
+# ---------------------------------------------------------------------
+
+class main(Agent):
+    def main(self):
+
+        # World initialization
+        init() >> [show_line("\nInitialiting Ontology...\n"), initWorld(), declareRules(), saveOnto()]
+
+        # Importing related triples
+        load() >> [show_line("\nAsserting all OWL 2 triples beliefs...\n"), assert_beliefs_triples(), show_line("\nTurning triples beliefs into Semas beliefs...\n"), turn()]
+        turn() / TRIPLE(X, "hasLedger",Z) >> [-TRIPLE(X,"hasLedger",Z), +LEDGER(X,"0"), AssignId(X), turn()]
+
+        # desires
+        setup() / WORKTIME(W) >> [show_line("Setup worktime again...\n"), load(), -WORKTIME(W), +WORKTIME(0)]
+        setup() >> [show_line("Setup worktime...\n"), +WORKTIME(0), +MAX_WORK_TIME(Max_Work_Time), +MAX_WORKDAY_TIME(Max_WorkDay_Time), +REST_TIME(Rest_Time)]
+        work() >> [show_line("Starting task detection...\n"), Timer(Max_Work_Time).start(), TaskDetect().start(), show_line("Workers on duty...")]
+
+        # AssignJob intentions
+        +TASK(X, Y) / (AGT(A, D) & DUTY(D)) >> [show_line("assigning job to ",A), -DUTY(D), +TASK(X, Y, A)[{'to': A}]]
+
+        # ReceiveCommunication intentions
+        +COMM(X)[{'from': W}] / LEDGER(W, H) >> [show_line("received job done comm from ", W), -LEDGER(W, H), UpdateLedger(W, H)]
+
+        # Pause work intentions - check if the whole working time belief WORKTIME is greater-equal than MAX_WORKDAY_TIME
+        +TIMEOUT("ON") / (WORKTIME(X) & MAX_WORKDAY_TIME(Y) & geq(X,Y)) >> [show_line("\nWorkers are very tired. Finishing working day.\n"), TaskDetect().stop(), stopwork()]
+
+        # End work intentions - Add the MAX_WORK_TIME quantity (during the pause) to the whole working time belief WORKTIME
+        +TIMEOUT("ON") / (WORKTIME(X) & MAX_WORK_TIME(Y)) >> [show_line("\nWorkers are tired, they need some rest.\n"), TaskDetect().stop(), -WORKTIME(X), UpdateWorkTime(X, Y), noduty()]
+        noduty() / (AGT(A, D) & DUTY(D)) >> [show_line("Putting agent" , A, " to rest..."), -DUTY(D), noduty()]
+        noduty() / REST_TIME(X) >> [rest(X), work()]
+
+        # Stop work intention
+        stopwork() / (AGT(A, D) & DUTY(D)) >> [show_line("\n-------------------------> Stopping ", A), -DUTY(D), stopwork()]
+        stopwork() >> [show_line("\nAll workers were stopped. Starting payment process."), pay()]
+
+        # pay desires
+        pay() / LEDGER(Z, H) >> [show_line("\nSending payment to ",Z, " for ",H," tasks..."), -LEDGER(Z, H), pay()]
+        pay() >> [show_line("\nPayments completed.")]
+
+        # Sending belief to agent
+        send(A, X) >> [show_line("Sending belief TASK(",X,") to agent ", A), +AGT(A), +TASK(X)]
+        +TASK(X) / AGT(A) >> [-AGT(A), +TASK(X)[{'to': A}]]
+
+        +TASK(X)[{'from': W}] >> [show_line("received belief from ", W), +TRIPLE(X, X, X)]
+
+
+# Custom agent
+instance = globals()["rocco"]()
+instance.start()
+
+main().start()
+
+# PHIDIAS.achieve(load(), "main")
+# PHIDIAS.achieve(setup(), "main")
+# PHIDIAS.achieve(work(), "main")
---
+++ immagene_mas.py
@@ -0,0 +1,6 @@
+from phidias.Lib import *
+from front_end_mas import *
+
+# run the engine shell
+PHIDIAS.shell(globals())
+
