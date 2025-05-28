709e9929be508c8193937cb4b5dc780379ffd4b5
fabio <fabiuslongo@gmail.com>
2025-05-28 13:53:47 +0200

    +mas

--- front_end_mas.py
+++ front_end_mas.py
@@ -17,8 +17,8 @@ def create_agent(class_name):
         +DESCR(D, P)[{'from': A}] / ack_plan(D, P) >> [-DESCR(D, P), show_line("\nReceived belief DESCR(",D,") from ", A), accept()]
         +DESCR(D, P)[{'from': A}] >> [-DESCR(D, P), show_line("\nReceived belief DESCR(",D,") from ", A), refuse()]
 
-        accept() >> [show_line("\n>>>>>>>> Accepting plan\n"), +ACK("TRUE")[{'to': 'main'}]]
-        refuse() >> [show_line("\n>>>>>>>> Refusing plan\n"), +ACK("FALSE")[{'to': 'main'}]]
+        accept() >> [show_line("\n>>>>>>>> Accepting plan <<<<<<<<<<\n"), +ACK("TRUE")[{'to': 'main'}]]
+        refuse() >> [show_line("\n>>>>>>>> Refusing plan <<<<<<<<<<\n"), +ACK("FALSE")[{'to': 'main'}]]
 
     return type(class_name, (Agent,), {"main": main})
 
@@ -44,16 +44,9 @@ class main(Agent):
         init() >> [show_line("\nAchieving img description. Waiting...\n"), achieve_img_descr(), setup()]
 
         setup() / DESCR(D) >> [show_line("\nImage description achieved: ", D), formulate_goal(D), achieve_plan()]
-
         achieve_plan() / (DESCR(D) & GOAL(G)) >> [show_line("\nPlanning for the goal: ", G, " from the description ", D), formulate_plan(D, G), commit()]
-
         commit() / (DESCR(D) & GOAL(G) & PLAN(P)) >> [+DESCR(D, P)[{'to': "metaval"}], show_line("\n>>>>>>>> Communication started <<<<<<<<<\n")]
-
         +ACK(X)[{'from': A}] >> [show_line(">>>>>>>> received ackowledgemt ",X," from ", A)]
-
-
-        #+PLAN(D, G, P) >> [show_line("\nActions implementation the plan ", P, " to achieve the goal ", G, " from the scenario ", D), formulate_action(D, G, P)]
-        #+ACTION(D, G, P, A) / ack_plan(D, P) >> [show_line("\nCommitting the action ", A, " implementing the plan ", P, ".")]
 
 main().start()
 
