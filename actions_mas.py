ea78f771e081622ca044cdb64bc8e220356e1e07
fabio <fabiuslongo@gmail.com>
2025-05-28 13:30:47 +0200

    +mas

---
+++ actions_mas.py
@@ -0,0 +1,303 @@
+import sys
+import queue
+from pymongo import MongoClient
+from ollama_inference import ask_ollama_stream, describe_image_status
+import json
+
+import sqlite3
+import numpy as np
+# pip install sentence-transformers
+from sentence_transformers import SentenceTransformer
+# pip install huggingface_hub[hf_xet]
+from sklearn.metrics.pairwise import cosine_similarity
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client['dipa']
+collection = db['annotations_collection']
+
+# Inizializza modello
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# Coda per inviare richieste di query
+query_queue = queue.Queue()
+
+# Coda per restituire i risultati delle query
+result_queue = queue.Queue()
+
+sys.path.insert(0, "../lib")
+
+from phidias.Lib import *
+from phidias.Agent import *
+from phidias.Types import *
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# LLM Multi-Modal Section
+MM_HOST = config.get('MULTI_LLM', 'HOST')
+MM_MODEL = config.get('MULTI_LLM', 'MODEL')
+MM_TEMP = config.get('MULTI_LLM', 'TEMP')
+MM_SYSTEM = config.get('MULTI_LLM', 'SYSTEM')
+
+# LLM text Section
+HOST = config.get('TEXT_LLM', 'HOST')
+MODEL = config.get('TEXT_LLM', 'MODEL')
+TEMP = config.get('TEXT_LLM', 'TEMP')
+SYSTEM = config.get('TEXT_LLM', 'SYSTEM')
+
+# Testing inference Section
+IMAGES_PATH = config.get('INFERENCE', 'IMAGES_PATH')
+IMAGES = config.get('INFERENCE', 'IMAGES_LIST')
+
+
+
+# Agent classes
+class init(Procedure): pass
+
+class accept(Procedure): pass
+
+class refuse(Procedure): pass
+
+class achieve_plan(Procedure): pass
+
+
+class commit(Procedure): pass
+
+class setup(Procedure): pass
+
+class PLAN(Belief): pass
+
+class GOAL(Reactor): pass
+
+class ACTION(Reactor): pass
+
+class DESCR(Belief): pass
+
+class ACK(Reactor): pass
+
+
+class formulate_goal(Action):
+    """Formulate goal from imahge description"""
+    def execute(self, arg):
+        # print(f"arg1: {arg}")
+        descr = str(arg).split("'")[3]
+        # print(f"descr: {descr}")
+
+        print("Formulating goal...")
+        goal = "[FORMULATED_GOAL]"
+        self.assert_belief(GOAL(descr, goal))
+
+
+class formulate_plan(Action):
+    """create sparql query from MST"""
+    def execute(self, arg1, arg2):
+
+        # print(f"arg1: {arg1}")
+        # print(f"arg2: {arg2}")
+
+        descr = str(arg1).split("'")[3]
+        goal = str(arg2).split("'")[3]
+
+        print("Formulating plan...")
+        plan = "[FORMULATED_PLAN]"
+        self.assert_belief(PLAN(descr, goal, plan))
+
+
+class formulate_action(Action):
+
+    """create sparql query from MST"""
+    def execute(self, arg1, arg2, arg3):
+
+        # print(f"arg1: {arg1}")
+        # print(f"arg2: {arg2}")
+        # print(f"arg3: {arg3}")
+
+        descr = str(arg1).split("'")[3]
+        goal = str(arg2).split("'")[3]
+        plan = str(arg3).split("'")[3]
+
+        print("Formulating action...")
+        action = "[FORMULATED_ACTION]"
+        self.assert_belief(ACTION(descr, goal, plan, action))
+
+
+
+class ack_plan(ActiveBelief):
+    """ActiveBelief for achieving acknowledgement from LLM for the current plan"""
+    def evaluate(self, arg1, arg2):
+
+        descr = str(arg1).split("'")[3]
+        plan = str(arg2).split("'")[3]
+
+        print(f"\nPlan {plan} assessment for the scenario {descr}...")
+
+        result = find_most_similar(descr)
+        print("\n🔍 Closer result:")
+        print(result)
+
+        file_to_search = result['file_image_name'].split(".")[0]
+        privacy_threatening_list = query_database(file_to_search)
+        print(f"\nPrivacy threatening items: {privacy_threatening_list}")
+
+        #SYSTEM_PROMPT = f""+SYSTEM
+
+        SYSTEM_PROMPT = f"In the following description, answer with a single boolean TRUE or FALSE, weather or not you found items (or similar) from the following privacy-threating list: {privacy_threatening_list}. The boolean must be followed by the number of found items (e.g TRUE 2). Report also which items you found."
+
+        meta_outcome = ask_ollama_stream(HOST, descr, SYSTEM_PROMPT, TEMP, MODEL)
+
+        # solo per modelli chain-of-thoughs (e.g. deedseek, qwen)
+        # meta_outcome = re.sub(r"<think>.*?</think>", "",  meta_outcome, flags=re.DOTALL)
+
+        #print(f"\nmeta-assessment: {meta_outcome}")
+
+        meta_outcome = meta_outcome.replace("\n", " ")
+
+        parti = meta_outcome.split(" ")
+
+        # Completa la lista con stringhe vuote se ha meno di 3 elementi
+        while len(parti) < 3:
+            parti.append("")
+
+        response = parti[0].strip()
+        features = parti[1].strip()
+        expl = ' '.join(parti[2:])
+
+        print(f"- Response: {response}")
+        print(f"- #Features found: {features}")
+        print(f"- Explanation: {expl}")
+
+        if response == "TRUE":
+            return True
+        else:
+            return False
+
+
+class compute_ack(Action):
+    """ActiveBelief for achieving acknowledgement from LLM for the current plan"""
+    def evaluate(self, arg1, arg2):
+
+        descr = str(arg1).split("'")[3]
+        plan = str(arg2).split("'")[3]
+
+        print(f"\nPlan {plan} assessment for the scenario {descr}...")
+
+        result = find_most_similar(descr)
+        print("\n🔍 Closer result:")
+        print(result)
+
+        file_to_search = result['file_image_name'].split(".")[0]
+        privacy_threatening_list = query_database(file_to_search)
+        print(f"\nPrivacy threatening items: {privacy_threatening_list}")
+
+        SYSTEM_PROMPT = f"In the following description, answer with a single boolean TRUE or FALSE, weather or not you found items (or similar) from the following privacy-threating list: {privacy_threatening_list}. The boolean must be followed by the number of found items (e.g TRUE 2). Report also which items you found."
+
+        meta_outcome = ask_ollama_stream(HOST, descr, SYSTEM_PROMPT, TEMP, MODEL)
+
+        # solo per modelli chain-of-thoughs (e.g. deedseek, qwen)
+        # meta_outcome = re.sub(r"<think>.*?</think>", "",  meta_outcome, flags=re.DOTALL)
+
+        #print(f"\nmeta-assessment: {meta_outcome}")
+
+        meta_outcome = meta_outcome.replace("\n", " ")
+
+        parti = meta_outcome.split(" ")
+
+        # Completa la lista con stringhe vuote se ha meno di 3 elementi
+        while len(parti) < 3:
+            parti.append("")
+
+        response = parti[0].strip()
+        features = parti[1].strip()
+        expl = ' '.join(parti[2:])
+
+        print(f"- Response: {response}")
+        print(f"- #Features found: {features}")
+        print(f"- Explanation: {expl}")
+
+        if response == "TRUE":
+            PHIDIAS.achieve(accept(), "metaval")
+        else:
+            PHIDIAS.achieve(refuse(), "metaval")
+
+
+
+
+################ Meta-Reasoning Section ################
+
+
+class achieve_img_descr(Action):
+    """Formulate goal from image description"""
+    def execute(self):
+
+        image_path = IMAGES_PATH+"/"+IMAGES
+        print(f"Img path: {image_path}")
+
+        success, descr = describe_image_status(MM_HOST, image_path, MM_SYSTEM, MM_TEMP, MM_MODEL)
+
+        if success:
+            #print(f"Img descr: {descr}")
+            self.assert_belief(DESCR(descr))
+        else:
+            print(descr)
+
+
+def find_most_similar(input_text):
+    input_embedding = model.encode(input_text).astype(np.float32)
+
+    conn = sqlite3.connect('inferences/image_descriptions_t08_34b_brief.db')
+    c = conn.cursor()
+    c.execute('SELECT id, file_image_name, description, embedding FROM immagini')
+    rows = c.fetchall()
+    conn.close()
+
+    best_score = -1
+    best_row = None
+
+    for row in rows:
+        row_id, file_name, desc, emb_blob = row
+        emb_array = np.frombuffer(emb_blob, dtype=np.float32)
+        score = cosine_similarity([input_embedding], [emb_array])[0][0]
+        if score > best_score:
+            best_score = score
+            best_row = {
+                "id": row_id,
+                "file_image_name": file_name,
+                "description": desc,
+                "similarity": round(score, 4)
+            }
+
+    return best_row
+
+
+
+def query_database(file_to_search):
+
+    file_prefix = file_to_search.split('_')[0]
+    result = collection.find_one({'file_name': {'$regex': f'^{file_prefix}'}})
+    no_privacy_false_categories = []
+
+    if result:
+        print(f"\n📄 Document found for {file_to_search}:\n")
+        result.pop('_id', None)
+        #print(json.dumps(result, indent=2, ensure_ascii=False))
+
+        default_annotation = result.get('defaultAnnotation', {})
+
+        if not default_annotation:
+            print("⚠️ No field 'defaultAnnotation' found in the document.")
+            return
+
+        for category_name, category_data in default_annotation.items():
+            if_no_privacy = category_data.get('ifNoPrivacy', None)
+            print(f"🧩 Categoria: {category_name} | ifNoPrivacy: {if_no_privacy}")
+
+            if if_no_privacy is False:
+                no_privacy_false_categories.append(category_name)
+    else:
+        print(f"\n❌ No documents found for {file_to_search}")
+
+    return no_privacy_false_categories
+
+
--- front_end_mas.py
+++ front_end_mas.py
@@ -1,18 +1,11 @@
 import time
-from actions import *
+from actions_mas import *
 
 # ---------------------------------------------------------------------
 # PHIDIAS rules variable declaration
 # ---------------------------------------------------------------------
 
 def_vars("D", "G",  "P", "A", "X")
-
-class DESCR(Belief): pass
-
-class ACK(Reactor): pass
-
-class commit(Procedure): pass
-
 
 # ---------------------------------------------------------------------
 # Agents section
@@ -21,7 +14,10 @@ class commit(Procedure): pass
 def create_agent(class_name):
     def main(self):
         # Custom intention
-        +DESCR(X)[{'from': A}] >> [-DESCR(X), show_line("\nReceived belief DESCR(",X,") from ", A), +ACK("TRUE")[{'to': 'main'}]]
+        +DESCR(D, P)[{'from': A}] >> [-DESCR(D, P), show_line("\nReceived belief DESCR(",D,") from ", A), compute_ack(D, P)]
+
+        accept() >> [show_line("\n>>>>>>>> Accepting plan\n"), +ACK("TRUE")[{'to': 'main'}]]
+        refuse() >> [show_line("\n>>>>>>>> Refusing plan\n"), +ACK("FALSE")[{'to': 'main'}]]
 
     return type(class_name, (Agent,), {"main": main})
 
@@ -44,13 +40,17 @@ instance.start()
 class main(Agent):
     def main(self):
 
-        init() >> [show_line("\nAchieving img description. Waiting...\n"), achieve_img_descr()]
+        init() >> [show_line("\nAchieving img description. Waiting...\n"), achieve_img_descr(), setup()]
 
-        +DESCR(D) >> [+DESCR(D)[{'to': "metaval"}], show_line("\nImage description achieved: ", D), formulate_goal(D)]
+        setup() / DESCR(D) >> [show_line("\nImage description achieved: ", D), formulate_goal(D), achieve_plan()]
 
-        +ACK(X)[{'from': A}] >> [show_line(">>>>>>>> received ackowledgemt from ", A)]
+        achieve_plan() / (DESCR(D) & GOAL(G)) >> [show_line("\nPlanning for the goal: ", G, " from the description ", D), formulate_plan(D, G), commit()]
 
-        #+GOAL(D, G) >> [show_line("\nPlanning for the goal: ", G, " from the description ", D), formulate_plan(D, G)]
+        commit() / (DESCR(D) & PLAN(P)) >> [+DESCR(D, P)[{'to': "metaval"}], show_line("\n>>>>>>>> Communication started <<<<<<<<<\n")]
+
+        +ACK(X)[{'from': A}] >> [show_line(">>>>>>>> received ackowledgemt ",X," from ", A)]
+
+
         #+PLAN(D, G, P) >> [show_line("\nActions implementation the plan ", P, " to achieve the goal ", G, " from the scenario ", D), formulate_action(D, G, P)]
         #+ACTION(D, G, P, A) / ack_plan(D, P) >> [show_line("\nCommitting the action ", A, " implementing the plan ", P, ".")]
 
